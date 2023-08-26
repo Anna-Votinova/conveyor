df27e8ccbdf11c03c10fe0c46cc98c9803d9a4a4
Anna-Votinova <anyvotinova@yandex.ru>
2023-08-27 02:53:36 +0300

    start dossier

---
+++ dossier/src/main/java/com/neoflex/dossier/DossierApplication.java
@@ -0,0 +1,13 @@
+package com.neoflex.dossier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DossierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DossierApplication.class, args);
+	}
+
+}
